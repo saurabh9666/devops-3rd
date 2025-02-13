bac58815229d0b10efce4a7c013dbdae3f61bdc1
Your Name <you@example.com>
2025-02-13 17:56:40 +0530

    Initial commit

---
+++ DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.c5esoao4o5x0.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin$123',
+    DB_DATABASE : 'webappdb'
+});
